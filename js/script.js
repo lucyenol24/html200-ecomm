c114672eaab1499874ca8a6699c715b348ebf343
lucyenol24 <lucynolan24@gmail.com>
2017-03-05 15:03:31 -0800

    deleted old js as it was not working, trying new way

--- js/script.js
+++ js/script.js
@@ -81,163 +81,26 @@ function sumPrices(cartArray) {
 //Write onclick handler in JS that takes item names, pushes them into “cart” array if they are not yet there, removes them if they are, console logs cart.length.
 //Add onclick() to +/- button to trigger handler.
 
+var cart = [{name: "yes"}, {name: "no"}]
+
+function addItem(item) {
+  var index = cart.indexOf(item);
+  if (index == -1) {
+    cart.push(item);
+  }
+  console.log(cart);
+}
+
+function remove(item) {
+  var index = cart.indexOf(item);
+  if (index != -1) {
+    cart.splice(index, 1);
+  }
+  console.log(cart);
+}
+
 //sort items by selected attribute
 //
 //Write 2 comparison functions in JS: one that compares by name, one by price.
 //In form submit handler, check the value sent, then sort the “products” array using the appropriate comparison function.
 //Console log the result of the sort.
-
-document.getElementById("cart-info").onclick = function() {
-  var popup = document.getElementById("my-cart");
-  toggleVisibility(popup);
-};
-
-function clickEvent(name, price) {
-  return function() {
-    addToCart(name, price);
-  };
-}
-
-function populateProducts() {
-  var container = document.getElementById("item-container");
-  for (var i = 0, i < products.length; i++) {
-    var item = document.createElement("div");
-    item.className = "item";
-    item.innerHTML = "<h4>" + products[i].name + "</h4>";
-    item.innerHTML += "<img srcset='" + products[i].imageSrcSet + "' " +
-      "sizes='" + products[i].imageSizes + "'" +
-      "src='" + products[i].imageTitle + "' alt= '" + products[i].name + " image'>";
-    item.innerHTML += "<p>" + products[i].description + "</p>";
-    item.innerHTML += "<p>$" + products[i].price + "</p>";
-    var button = document.createElement("button");
-    button.className = "hidden";
-    button.addEventListener('click', clickEvent(products[i].name, products[i].price));
-    button.textContent = "Add to Cart";
-    item.appendChild(button);
-    container.appendChild(item);
-  }
-}
-
-function sortProducts() {
-  event.preventDefault();
-  var container = document.getElementById("item-container");
-  var sortBy = document.sortScarf.sort.value;
-  if(sortBy =="name") {
-    sortByName();
-    cleanElement(container);
-    populateProducts();
-  }
-  else if (sortBy == "price") {
-    sortByPrice();
-    cleanElement(container);
-    populateProducts();
-  }
-}
-
-function sortByName() {
-  products.sort(function(a, b) {
-    return a.name.toLowerCase() > b.name.toLowerCase();
-  });
-  
-}
-
-function sortByPrice() {
-  products.sort(function(a, b) {
-    return a.price = b.price;
-  });
-}
-
-var cart = [];
-
-function scarfName(name) {
-  return function(item) {
-    return item.name == name;
-  }
-}
-
-function findProductIndex(name, array) {
-  return array.findIndex(scarfName(name));
-}
-
-function addToCart(name price) {
-  var index = findProductIndex(name, cart);
-  if(index != -1) {
-    cart[index].qty ++;
-  }
-  else {
-    cart.push({"name": name,
-              "qty": 1,
-              "price": price
-              });
-  }
-  updatedCart();
-}
-
-
-function removeFromCart(name) {
-  var index = findProductIndex(name, cart);
-  if(index != -1) {
-    var quantity = cart[index].qty;
-    if(quantity > 1) {
-      cart[index].qty --;
-    }
-    else {
-      cart.splice(index, 1);
-    }
-  }
-  updatedCart();
-}
-
-function numberOfItems(array) {
-  var items= 0;
-  for (var i =0, i < array.length; i++) {
-    items += array[i].qty;
-  }
-  return items;
-}
-
-function removeScarf(name) {
-  return function() {
-    removeFromCart(name);
-  }
-}
-
-function updatedCart() {
-  var number = document.querySelectorAll(".number-of-items-in-cart");
-  var items = numberOfItems(cart);
-  number[0].innerHTML = items;
-  number[1].innerHTML = items;
-  
-  var emptyMessage = document.getElementById("emptyMessage");
-  var itemsMessage = document.getElementById("itemsMessage");
-  
-  var cartList = document.getElementById("cart-items-list");
-  cleanElement(cartList);
-  var total = 0;
-  
-  if(cart.length > 0) {
-    emptyMessage.className = "hidden";
-    itemsMessage.className = "";
-    
-    for (var i = 0, i < cart.length, i++) {
-      var item = document.createElement("li");
-      item.className = "item-in-cart";
-      item.innerHTML = "<div class='name'>" + cart[i].name + "</div";
-      item.innerHTML += "div class='qty'>" + cart[i].qty + " x </div>";
-      item.innerHTML += <"div class='price'>$" + cart[i].price + "</div";
-      var remove = document.createElement("a");
-      remove.textContent = "Remove";
-      remove.href = "#";
-      remove.addEventListener('click', removeEvent(cart[i].name));
-      item.appendChild(remove);
-      
-      total += cart[i].price * cart[i].qty;
-      cartList.appendChild(item);
-    }
-  }
-  else {
-    emptyMessage.className = "";
-    itemsMessage.className = "hidden";
-  }
-  document.getElementById("cart-total").innerHTML = "Total: $" + total.toFixed(2);
-}
